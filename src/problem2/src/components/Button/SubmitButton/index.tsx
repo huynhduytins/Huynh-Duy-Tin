737da7bc08a2284b8e85fae549575489351085b2
Huỳnh Duy Tín <hduytins@gmail.com>
2023-10-23 20:35:17 +0700

    fix: border radius loading button

--- src/problem2/src/components/Button/SubmitButton/index.tsx
+++ src/problem2/src/components/Button/SubmitButton/index.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@mui/material'
-import { useMode } from '../../../context/AppContext'
+import { Button } from "@mui/material";
+import { useMode } from "../../../context/AppContext";
 
 const SubmitButton = () => {
-  const { isSwapping, setIsSwapping, handleSubmitSwap } = useMode()
+  const { isSwapping, setIsSwapping, handleSubmitSwap } = useMode();
 
   return isSwapping ? (
-    <button className="flex justify-center items-center gap-2 w-full h-12 bg-light-500 rounded-2xl text-dark-400 dark:text-white">
+    <button className="flex justify-center items-center gap-2 w-full h-12 bg-light-500 rounded-[0.7rem] text-dark-400 dark:text-white">
       <span className="">Loading</span>
       <div className="flex justify-center items-center bg-gradient-to-t from-light-500  to-dark-400 dark:to-white w-7 h-7 rounded-full animate-spin">
         <div className="bg-light-500 w-6 h-6 rounded-full" />
@@ -16,22 +16,22 @@ const SubmitButton = () => {
       variant="contained"
       className="w-full h-12  dark:text-dark-400"
       sx={{
-        borderRadius: '0.7rem',
-        backgroundColor: '#7B8EC8',
-        ':hover': {
-          backgroundColor: '#869bda',
+        borderRadius: "0.7rem",
+        backgroundColor: "#7B8EC8",
+        ":hover": {
+          backgroundColor: "#869bda",
         },
       }}
       onClick={() => {
-        setIsSwapping(true)
-        handleSubmitSwap()
+        setIsSwapping(true);
+        handleSubmitSwap();
       }}
     >
       <span className="dark:text-dark-400 font-bold font-kanit">
         CONFIRM SWAP
       </span>
     </Button>
-  )
-}
+  );
+};
 
-export default SubmitButton
+export default SubmitButton;
